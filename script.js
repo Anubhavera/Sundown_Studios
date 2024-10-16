c19a861d47667fc9668326446018c986f5033c3c
ANUBHAV HOODA <102412479+Anubhavera@users.noreply.github.com>
2024-10-17 00:34:35 +0530

    Refactor HTML and JavaScript code, fix script.js reference, and add loader and fixed-image elements

--- script.js
+++ script.js
@@ -2,3 +2,46 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true,
 })
+
+// var a = document.querySelector("#elem1")
+// var abc = a.getAttribute('data-image')
+
+var elemC = document.querySelector("#elem-container")
+var fixed = document.querySelector("#fixed-image")
+elemC.addEventListener("mouseenter", function () {
+  fixed.style.display = "block"
+})
+elemC.addEventListener("mouseleave", function () {
+  fixed.style.display = "none"
+})
+
+// var elems = document.querySelectorAll(".elem")
+// elems.forEach(function (e) {
+//   e.addEventListener("mouseenter", function () {
+//     var Image = e.getAttribute("data-image")
+//     fixed.style.backgroundImage = `url(${Image})`
+//   })
+// })
+
+var elems = document.querySelectorAll(".elem")
+elems.forEach(function (e) {
+  e.addEventListener("mouseenter", function () {
+    var Image = e.getAttribute("data-image")
+    fixed.style.backgroundImage = `url(${Image})`
+  })
+})
+
+// function menuAnimation(){
+
+// }
+
+var loader = document.querySelector("#loader")
+setTimeout(() => {
+  loader.style.top = "-100%"
+}, 0000)
+
+var ima = document.querySelector(".elem")
+ima.addEventListener("mouseenter", function () {
+  var ui = ima.getAttribute("img-src")
+  fixed.style.backgroundImage = `url(${ui})`
+})
